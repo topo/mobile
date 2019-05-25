dd5e8b8db99b89d6ccf74506087d076ba01f6112
the-duck <markspurgeon96@hotmail.com>
2019-05-26 00:32:03 +0200

    service worker fetch

--- serviceworker.js
+++ serviceworker.js
@@ -18,3 +18,11 @@ self.addEventListener('install', function(event) {
   );
 
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
