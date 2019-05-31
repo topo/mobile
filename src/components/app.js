46dbb2b2f2684a5af670a17cd32c43e246e93094
the-duck <markspurgeon96@hotmail.com>
2019-05-31 10:53:02 +0200

    code-split: posts to dedicated file

--- src/components/app.js
+++ src/components/app.js
@@ -2,106 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { disableBodyScroll } from 'body-scroll-lock';
 
+import Posts from './posts';
 
-// Stolen from https://gist.github.com/gre/1650294
-/*eslint-disable */
-function easeOutCubic(t) { return (--t) * t * t + 1; }
-/*eslint-disable */
-
-// Animate scroll
-function updateScroll(id) {
-  const el = document.getElementById(id);
-  if (el) {
-    let currentOffset = window.pageYOffset||document.documentElement.scrollTop||0;
-    const targetOffset = el.offsetTop || 0;
-    const scrollDifference = targetOffset - currentOffset;
-
-    let scrollPosition = currentOffset;
-    const increment = 0.02;
-    let position = 0; // percentage (0 to 1)
-
-    // Animation loop
-    const animation = requestAnimationFrame(goToTarget);
-
-    function goToTarget() {
-      position += increment;
-      const positionInFunction = easeOutCubic(position);
-
-      scrollPosition = currentOffset + positionInFunction * scrollDifference;
-
-      if (positionInFunction < 1) {
-        window.scrollTo(0, scrollPosition);
-        requestAnimationFrame(goToTarget);
-      } else {
-        cancelAnimationFrame(animation);
-        // window.scrollTo(0,targetOffset)
-        currentOffset = scrollPosition; // dunno why, but this has to be updated
-      }
-    }
-  }
-}
-
-const Loading = () => {
-  return (
+const Loading = () => (
     <div className="post loading">
       <div className="post-content">
         <p>Ça réfléchit ... Attends quelques petites secondes !</p>
       </div>
     </div>
-  )
-}
-
-const CoAuthors = ({coauthors}) => coauthors.map((author) => {
-  if (author.avatar && !author.avatar.startsWith('https://topolitique.ch')) {
-    author.avatar = "assets/coauthor.png";
-  };
-  return (
-    <span key={author.name} >
-      <img className="author-image" src={author.avatar} alt="-"/>
-      <span className="author">{author.name}</span>
-    </span>
-  );
-});
-
-const PostsContainer = ({ posts, activePost }) => posts.map((post, index) => {
-
-  const id = `post-${index}`;
-
-  if (activePost === index) { updateScroll(id); }
-
-  return (
-      <div
-        className={`post ${(activePost === index) ? 'active' : 'inactive'}`}
-        key={id}
-        id={id}
-        style={ (post.image && post.image.startsWith('https://')) ? { backgroundImage: `url(${post.image})` } : null}>
-
-        <div className="post-container">
-          <a className="post-content" href={post.link}>
-            <h2 className="kicker">{post.meta.kicker}</h2>
-            <br />
-            <h1 className="title">{post.title.rendered}</h1>
-            <p className="description">
-              <span
-                dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
-              />
-              <CoAuthors coauthors={post.coauthors} />
-            </p>
-          </a>
-        </div>
-
-      </div>
-  );
-});
-const Posts = connect(
-  state => ({
-    activePost: state.post,
-    posts: state.posts,
-  }),
-)(PostsContainer, 'Posts');
+);
 
 
-const App = ({posts}) => {
+const App = ({ posts }) => {
   const targetRef = React.createRef();
   const targetElement = targetRef.current;
 
@@ -109,10 +21,8 @@ const App = ({posts}) => {
   disableBodyScroll(targetElement);
 
   let items = <Loading />;
-  if (posts.length>0) {
-    try {
-      items = <Posts />;
-    } catch (e) {}
+  if (posts.length > 0) {
+    items = <Posts />;
   }
 
   return (
@@ -123,5 +33,5 @@ const App = ({posts}) => {
   );
 };
 export default connect(
-  state => ({posts:state.posts})
+  state => ({ posts: state.posts }),
 )(App, 'App');
---
+++ src/components/posts.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+// Stolen from https://gist.github.com/gre/1650294
+/*eslint-disable */
+const easeOutCubic = (t) => { return (--t) * t * t + 1; }
+/*eslint-disable */
+
+// Animate scroll
+const updateScroll = (id) => {
+  const el = document.getElementById(id);
+  if (el) {
+    let currentOffset = window.pageYOffset||document.documentElement.scrollTop||0;
+    const targetOffset = el.offsetTop || 0;
+    const scrollDifference = targetOffset - currentOffset;
+
+    let scrollPosition = currentOffset;
+    const increment = 0.02;
+    let position = 0; // percentage (0 to 1)
+
+    // Animation loop
+    const animation = requestAnimationFrame(goToTarget);
+
+    function goToTarget() {
+      position += increment;
+      const positionInFunction = easeOutCubic(position);
+
+      scrollPosition = currentOffset + positionInFunction * scrollDifference;
+
+      if (positionInFunction < 1) {
+        window.scrollTo(0, scrollPosition);
+        requestAnimationFrame(goToTarget);
+      } else {
+        cancelAnimationFrame(animation);
+        // window.scrollTo(0,targetOffset)
+        currentOffset = scrollPosition; // dunno why, but this has to be updated
+      }
+    }
+  }
+}
+
+const CoAuthors = ({coauthors}) => coauthors.map((author) => {
+  if (author.avatar && !author.avatar.startsWith('https://topolitique.ch')) {
+    author.avatar = "assets/coauthor.png";
+  };
+  return (
+    <span key={author.name} >
+      <img className="author-image" src={author.avatar} alt="-"/>
+      <span className="author">{author.name}</span>
+    </span>
+  );
+});
+
+const PostsContainer = ({ posts, activePost }) => posts.map((post, index) => {
+
+  const id = `post-${index}`;
+
+  if (activePost === index) { updateScroll(id); }
+
+  return (
+      <div
+        className={`post ${(activePost === index) ? 'active' : 'inactive'}`}
+        key={id}
+        id={id}
+        style={ (post.image && post.image.startsWith('https://')) ? { backgroundImage: `url(${post.image})` } : null}>
+
+        <div className="post-container">
+          <a className="post-content" href={post.link}>
+            <h2 className="kicker">{post.meta.kicker}</h2>
+            <br />
+            <h1 className="title">{post.title.rendered}</h1>
+            <p className="description">
+              <span
+                dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
+              />
+              <CoAuthors coauthors={post.coauthors} />
+            </p>
+          </a>
+        </div>
+
+      </div>
+  );
+});
+export default connect(
+  state => ({
+    activePost: state.post,
+    posts: state.posts,
+  }),
+)(PostsContainer, 'Posts');
